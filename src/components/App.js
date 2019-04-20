78613f2fac91f63ce8e42b16b243bc61bab80431
jayanth0107 <bujji.0107@gmail.com>
2019-04-20 13:45:24 +0530

    Code files commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            Jayanth React project deployed globally - Test
+        </div>
+    );
+};
+
+export default App;
