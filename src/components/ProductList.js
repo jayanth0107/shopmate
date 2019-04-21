5be4f9924ee59b3483fa523753cb1e754c1b188e
jayanth0107 <bujji.0107@gmail.com>
2019-04-22 01:52:55 +0530

    Updated ProductItems with full product information

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -11,24 +11,34 @@ class ProductList extends React.Component {
     }
 
     renderList(){
-        
+
         return this.props.products.map(product => {
             return(  
-                
+                <div className="card">
+                    <div className="content">
+                        <div className="header">{product.name}</div>                    
+                    </div>              
                     <div className="image">
-                        <img src={'https://backendapi.turing.com/images/products/' + product.thumbnail}/>
-                    </div>                          
+                        <img alt={product.name} src={'https://backendapi.turing.com/images/products/' + product.thumbnail}/>
+                    </div>  
+                    <div className="ui tag labels">
+                        <a className="ui red label">${product.price}</a>
+                        <a className="ui strikethrough label">${product.discounted_price}</a>
+                    </div>
+                    <div className="description">
+                        {product.description}
+                    </div> 
+                </div>                  
             )
         })
     }
 
     render(){  
-        //console.log(this.props.products); returns only 20
-        const {description, urls} = this.props.products;
+        //console.log(this.props.products); //returns only 20
+        
         return (
-            <div className="ui link cards">  
-                          
-                    {this.renderList()}
+            <div className="ui link cards">                    
+                    {this.renderList()}                                    
             </div>
                 
         )
