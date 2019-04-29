90f99552b7c02a132502d1a8d53cf7286b956e62
jayanth0107 <bujji.0107@gmail.com>
2019-04-29 22:43:53 +0530

    Updates

---
+++ precache-manifest.abafb29088663f4724a8e660283f6fb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f89ab9f7d8da43b71c67",
+    "url": "/shopmate/static/js/runtime~main.03f445c5.js"
+  },
+  {
+    "revision": "679fd18987214ed951ba",
+    "url": "/shopmate/static/js/main.f8d609b6.chunk.js"
+  },
+  {
+    "revision": "d91e058f50b5a2075524",
+    "url": "/shopmate/static/js/2.3743d3dc.chunk.js"
+  },
+  {
+    "revision": "679fd18987214ed951ba",
+    "url": "/shopmate/static/css/main.ba079f85.chunk.css"
+  },
+  {
+    "revision": "3eaf27b8214506c1538784857628fad5",
+    "url": "/shopmate/index.html"
+  }
+];
