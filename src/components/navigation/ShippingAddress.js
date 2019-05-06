2875d43ab8896b28ca5b8c0b261b0c83991c1548
jayanth0107 <bujji.0107@gmail.com>
2019-05-06 15:01:35 +0530

    billing address duplicate removal

--- src/components/navigation/ShippingAddress.js
+++ src/components/navigation/ShippingAddress.js
@@ -55,61 +55,11 @@ const ShippingAddress = () => {
                 
     </div>
   </div>
-  <h4 className={`ui dividing header shippingHeader`}>Billing Information</h4>
-        <div className="field">         
-
-            <select className="ui fluid dropdown">
-                    <option value="">Card Type</option>
-                <option value="AL">Visa</option>
-                <option value="AK">American Express</option>
-                <option value="AZ">Discover</option>
-            </select>
-        </div>
-        <div className="fields">
-            <div className="seven wide field">
-            <label htmlFor="card-element">Card Number</label>
-            <input type="text" name="card[number]" maxLength="16" placeholder="Card #"/>
-            </div>
-            <div className="three wide field">
-            <label>CVC</label>
-            <input type="text" name="card[cvc]" maxLength="3" placeholder="CVC"/>
-            </div>
-            <div className="six wide field">
-            <label>Expiration</label>
-            <div className="two fields">
-                <div className="field">
-                <select className="ui fluid search dropdown" name="card[expire-month]">
-                    <option value="">Month</option>
-                    <option value="1">January</option>
-                    <option value="2">February</option>
-                    <option value="3">March</option>
-                    <option value="4">April</option>
-                    <option value="5">May</option>
-                    <option value="6">June</option>
-                    <option value="7">July</option>
-                    <option value="8">August</option>
-                    <option value="9">September</option>
-                    <option value="10">October</option>
-                    <option value="11">November</option>
-                    <option value="12">December</option>
-                </select>
-                </div>
-                <div className="field">
-                <input type="text" name="card[expire-year]" maxLength="4" placeholder="Year"/>
-                </div>
-            </div>
-            </div>
-        </div>        
-   
-   
+ 
         <Link to="/billingAddress" className="item">
             <div className={`ui button shippingSubmit`} tabIndex="0">Submit Order</div>
         </Link>
-                        
-                        
-
-
-
+   
             </form>
         </div>
     ) 
